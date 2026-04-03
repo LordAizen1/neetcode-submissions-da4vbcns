febcdc88b7e44ef9605e8a8125c4109078603193
Md Kaif <119096690+LordAizen1@users.noreply.github.com>
2026-04-03 23:39:50 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        if(strs.empty()) return {""};
+        string encoded_string = "";
+        for(string s: strs){
+            encoded_string+=to_string(s.length())+'#'+s;
+        }
+        return encoded_string;
+    }
+    // 5#Hello5#World -> i=0 :5 , len = stoi(substr):5
+    // substr(j+1, len) : Hello
+    vector<string> decode(string s) {
+        vector<string> result;
+         int i=0; 
+        while(i<s.length()){
+            int j = s.find('#', i);
+            int len = stoi(s.substr(i, j-i));
+            result.push_back(s.substr(j+1, len));
+            i = j+len+1;
+        }
+        return result;
+    }
+};
