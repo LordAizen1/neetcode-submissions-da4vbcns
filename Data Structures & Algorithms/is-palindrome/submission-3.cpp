e329906f639f61bb02dda385d160704f8b3e7fc3
Md Kaif <119096690+LordAizen1@users.noreply.github.com>
2026-04-05 22:03:33 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string cleaned = "";
+        for(char ch: s){
+            if(isalnum(ch)){
+                cleaned+=tolower(ch);
+            }
+        }
+        string rev = cleaned;
+        reverse(rev.begin(), rev.end());
+        
+        return rev==cleaned;
+    }
+};
